b8c6bfc6a424945c94a4dbe81c82c66c26103344
a2z453 <braydenaza.07@gmail.com>
2025-06-13 17:19:57 +0000

    Vite Please Work

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    open: true,
+  root: '.', // important!
+  publicDir: 'public',
+  build: {
+    outDir: 'dist',
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
   },
 });
